ad61439ab4d7779bb08b5464925c3df7931f60d8
JamesBremner <james@ravenspoint.com>
2023-02-11 12:07:16 -0500

    Use wasted moves before sequence start #1

--- src/main.cpp
+++ src/main.cpp
@@ -124,8 +124,76 @@ void displayFoundSequence(const std::vector<int> &foundSequence)
     }
     std::cout << "\n";
 }
+/// @brief Find sequence with given start point
+/// @param[in] seqNo index of sequence sought
+/// @param[in] foundSequence vector with index of starting cell
+/// @param[out] foundSequence vector with indices of cells in sequence, or empty on failure
+/// @param[in] vert true if previous move was vertical
+/// @return true if sequence found
 
-std::vector<int> findSequence(int seqNo)
+bool findSequenceFromStart(
+    int seqNo,
+    std::vector<int> &foundSequence,
+    bool vert)
+{
+    auto foundSequencebackup = foundSequence;
+
+    int w, h;
+    pA->size(w, h);
+    bool found = true;
+    while (found)
+    {
+        // toggle search direction
+        vert = (!vert);
+
+        // start from last cell found
+        auto pmCell = pA->cell(foundSequence.back());
+        int c, r;
+        pA->coords(c, r, pmCell);
+
+        // look for next value in required sequence
+        std::string nextValue = vSequence[seqNo][foundSequence.size()];
+        found = false;
+
+        if (vert)
+        {
+            // loop over cells in column
+            for (int r2 = 1; r2 < w; r2++)
+            {
+                if (pA->cell(c, r2)->value == nextValue)
+                {
+                    foundSequence.push_back(pA->cell(c, r2)->ID());
+                    found = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // loop over cells in row
+            for (int c2 = 0; c2 < h; c2++)
+            {
+                if (pA->cell(c2, r)->value == nextValue)
+                {
+                    foundSequence.push_back(pA->cell(c2, r)->ID());
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) {
+            foundSequence = foundSequencebackup;
+            return false;
+        }
+
+        if (foundSequence.size() == vSequence[seqNo].size())
+            return true;
+    }
+    throw std::runtime_error(
+        "Should never come here");
+}
+
+std::vector<int> findSequenceStart(int seqNo)
 {
     raven::set::cRunWatch aWatcher("findSequence");
 
@@ -144,65 +212,46 @@ std::vector<int> findSequence(int seqNo)
         if (pA->cell(c, 0)->value == vSequence[seqNo][0])
         {
             foundSequence.push_back(pA->cell(c, 0)->ID());
-            found = true;
-        }
 
-        while (found)
-        {
-            // found possible starting cell
-
-            // toggle search direction
-            vert = (!vert);
-
-            // start from last cell found
-            auto pmCell = pA->cell(foundSequence.back());
-            int c, r;
-            pA->coords(c, r, pmCell);
-
-            // look for next value in required sequence
-            std::string nextValue = vSequence[seqNo][foundSequence.size()];
-            found = false;
-
-            if (vert)
+            if (findSequenceFromStart(
+                    seqNo,
+                    foundSequence,
+                    vert))
             {
-                // loop over cells in column
-                for (int r2 = 1; r2 < w; r2++)
-                {
-                    if (pA->cell(c, r2)->value == nextValue)
-                    {
-                        foundSequence.push_back(pA->cell(c, r2)->ID());
-                        found = true;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                // loop over cells in row
-                for (int c2 = 0; c2 < h; c2++)
-                {
-                    if (pA->cell(c2, r)->value == nextValue)
-                    {
-                        foundSequence.push_back(pA->cell(c2, r)->ID());
-                        found = true;
-                        break;
-                    }
-                }
-            }
-            if (!found)
-            {
-                // dead end - try starting from next cell in first row
-                break;
-            }
-
-            if (foundSequence.size() == vSequence[seqNo].size())
-            {
-
-                // success!!!
                 return foundSequence;
             }
         }
     }
+
+    std::cout << "Cannot find sequence starting in 1st row, using wasted moves\n";
+
+    for (int c = 0; c < w; c++)
+    {
+        for (int r = 1; r < h; r++)
+        {
+            if (pA->cell(c, r)->value == vSequence[seqNo][0])
+            {
+                foundSequence.push_back(pA->cell(c, r)->ID());
+                vert = false;
+                if (findSequenceFromStart(
+                        seqNo,
+                        foundSequence,
+                        vert))
+                {
+                    return foundSequence;
+                }
+                vert = true;
+                if (findSequenceFromStart(
+                        seqNo,
+                        foundSequence,
+                        vert))
+                {
+                    return foundSequence;
+                }
+            }
+        }
+    }
+
     std::cout << "Cannot find sequence\n";
     foundSequence.clear();
     return foundSequence;
@@ -219,13 +268,12 @@ main()
 
     for (int seqNo = 0; seqNo < vSequence.size(); seqNo++)
     {
-
         std::cout << "for sequence ";
         for (auto &v : vSequence[seqNo])
             std::cout << v << " ";
         std::cout << "\n\n";
         displayFoundSequence(
-            findSequence(seqNo));
+            findSequenceStart(seqNo));
     }
 
     raven::set::cRunWatch::Report();
